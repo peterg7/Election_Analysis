6841121e011648160ea584617ff0961a950b7ae0
Peter Gish <pgish97@gmail.com>
2019-10-17 16:56:58 -0700

    adding the election audit code

--- PyPoll.py
+++ PyPoll.py
@@ -18,13 +18,83 @@ data_file_path = os.path.join('Resources', 'election_results.csv')
 # output analysis file path
 analysis_file_path = os.path.join('analysis', 'election_analysis.txt')
 
+# initialize total votes counter
+total_votes = 0
+
+# list of candidates
+candidate_options = []
+
+# dictionary for candidate: votes
+candidate_votes = {}
+
+# winning candidate and count tracker
+winning_candidate = ''
+winning_count = 0
+winning_percentage = 0
 
 # open data file for read
 with open(data_file_path) as election_data:
 
-	# read the file 
+	# create reader object
 	file_reader = csv.reader(election_data)
 	
+	# read header row
 	headers = next(file_reader)
 
-	
+	for row in file_reader:
+		total_votes += 1 # increment total votes
+
+		candidate_name = row[2]
+		if candidate_name not in candidate_options: 
+			# if it's a new candidate, add to list
+			candidate_options.append(candidate_name)
+			# begin tracking votes for candidate
+			candidate_votes[candidate_name] = 0
+
+		# increment this candidates # of votes
+		candidate_votes[candidate_name] += 1
+
+# go through candidates
+for candidate in candidate_votes:
+	# retrieve vote count
+	votes = candidate_votes[candidate]
+	# calculate percentage
+	vote_percentage = int(votes) / int(total_votes) * 100
+
+	print(f'{candidate}: {vote_percentage:.1f}% ({votes:,})')
+
+	# update winner trackers
+	if (votes > winning_count) and (vote_percentage > winning_percentage):
+		winning_count = votes
+		winning_percentage = vote_percentage
+		winning_candidate = candidate
+
+
+
+winning_candidate_summary = (
+	f'-----------------------\n'
+	f'Winner: {winning_candidate}\n'
+	f'Winning Vote Count: {winning_count:,}\n'
+	f'Winning Percentage: {winning_percentage:.1f}%\n'
+	f'-----------------------\n')
+print(winning_candidate_summary)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
